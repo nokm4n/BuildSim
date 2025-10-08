b732fdd32dff5f16796a6b9ee30e762e51218fc0
nokm4n <44049305+nokm4n@users.noreply.github.com>
2025-10-08 16:59:21 +0400

    Update saves

--- Assets/Scripts/Utility/SaveSystem.cs
+++ Assets/Scripts/Utility/SaveSystem.cs
@@ -80,23 +80,3 @@ public struct BuildingSaveData
 	public Vector3 origin;
 	public BuildingType type;
 }
-public static class JsonHelper
-{
-	[System.Serializable]
-	private class Wrapper<T>
-	{
-		public List<T> Items;
-	}
-
-	public static string ToJson<T>(List<T> list, bool prettyPrint = false)
-	{
-		Wrapper<T> wrapper = new Wrapper<T> { Items = list };
-		return JsonUtility.ToJson(wrapper, prettyPrint);
-	}
-
-	public static List<T> FromJson<T>(string json)
-	{
-		Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
-		return wrapper?.Items ?? new List<T>();
-	}
-}
